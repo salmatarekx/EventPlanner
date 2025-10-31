a2f0d024b2f0850845920f89029e5e5616e5c457
moyasser20 <mohamedyasser192023@gmail.com>
2025-10-31 20:31:42 +0200

    finish phase 0

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from routes.auth_routes import auth_router
 from database import connect_to_mongo
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
-    title="EventPlanner API",
-    version="1.0.0"
-)
+app = FastAPI(title="EventPlanner API", version="1.0.0")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:4200"],
@@ -18,25 +16,5 @@ app.add_middleware(
 @app.on_event("startup")
 def startup_db_client():
     connect_to_mongo()
-
-@app.get("/")
-def root():
-    return {
-        "message": "Welcome to EventPlanner API",
-        "version": "1.0.0",
-        "phase": "Phase 0 - User Management",
-        "status": "active"
-    }
-
-@app.get("/test-db")
-def test_db():
-    from database import db
-    try:
-        if db is None:
-            return {"error": "Database not connected"}
-        db.list_collection_names()
-        return {"message": "MongoDB connection successful!"}
-    except Exception as e:
-        return {"error": str(e)}
 
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
--- utils/auth_utils.py
+++ utils/auth_utils.py
@@ -7,19 +7,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY missing from .env file")
+
+SECRET_KEY = SECRET_KEY.strip()
+print("auth_utils loaded, SECRET_KEY =", SECRET_KEY, "| type:", type(SECRET_KEY))
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def hash_password(password: str):
     return pwd_context.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def create_access_token(data: dict):
+    print("⚡ create_access_token CALLED")
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    print("DEBUG SECRET_KEY =", SECRET_KEY, "| type:", type(SECRET_KEY))
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    print("Token created successfully!")
+    return token
