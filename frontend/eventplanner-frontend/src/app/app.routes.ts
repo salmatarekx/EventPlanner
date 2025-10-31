dcf7bd547e76147a29b3cd78bdd696d3328f40f4
moyasser20 <mohamedyasser192023@gmail.com>
2025-10-31 16:10:50 +0200

    add home page

--- frontend/eventplanner-frontend/src/app/app.routes.ts
+++ frontend/eventplanner-frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
 ];
+
+
+
